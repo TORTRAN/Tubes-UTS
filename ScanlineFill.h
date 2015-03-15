a447c84a6fc25a57ea9c3843d6efc3b6f5b61a4d
fahziar <fahziar_rw@yahoo.co.uk>
2015-03-15 21:55:23 +0700

    Penambahan cara penggunaan

--- ScanlineFill.h
+++ ScanlineFill.h
@@ -12,7 +12,22 @@
 class ScanlineFill
 {
 public:
+	/*
+	 * arrPoint berisi kumpulan titik yang membentuk poligon 
+	 * Contoh: Ada poligon berbentuk persegi panjang, Isi arrPoint:
+	 * arrPoint[0] = (100, 100)
+	 * arrPoint[1] = (200, 100)
+	 * arrPoint[3] = (200, 300)
+	 * arrPoint[4] = (100, 300)
+	*/
 	static void fill(std::vector <Point> arrPoint, Color color, FrameBuffer &fb);
+
+	/*
+	 * arrPoint sama dengan fungsi fill di atas
+	 * Pattern diiisi sama pattern
+	 * Contoh: 	fill dengan pattern kayu
+	 * 			fill(arrPoint, new PatternKayu(), fb)
+	*/
 	static void fill(std::vector <Point> arrPoint, Pattern * pattern, FrameBuffer &fb);
 };
 #endif
